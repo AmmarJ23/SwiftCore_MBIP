8e960ca3946d1a88abbb509ee27b71d4e3ababda
AmmarJ23 <ammar.jmldn@outlook.com>
2024-02-01 14:19:54 +0800

    commit

---
+++ MBIP_SwiftCore/src/com/model/User.java
@@ -0,0 +1,69 @@
+package com.model;
+
+public class User {
+		
+		String username;
+		String email;
+		String password;
+		String address;
+		String state;
+		String postcode;
+		String noResidents;
+		String buildingType;
+		String userType;
+		
+		public String getUsername() {
+			return username;
+		}
+		public void setUsername(String username) {
+			this.username = username;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getAddress() {
+			return address;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public String getState() {
+			return state;
+		}
+		public void setState(String state) {
+			this.state = state;
+		}
+		public String getPostcode() {
+			return postcode;
+		}
+		public void setPostcode(String postcode) {
+			this.postcode = postcode;
+		}
+		public String getNoResidents() {
+			return noResidents;
+		}
+		public void setNoResidents(String noResidents) {
+			this.noResidents = noResidents;
+		}
+		public String getBuildingType() {
+			return buildingType;
+		}
+		public void setBuildingType(String buildingType) {
+			this.buildingType = buildingType;
+		}
+		public String getUserType() {
+			return userType;
+		}
+		public void setUserType(String userType) {
+			this.userType = userType;
+		}
+}
